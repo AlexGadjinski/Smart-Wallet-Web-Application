90bfddb797180473e0881efdcecc1d8d361556b3
Aleksandar Gadzhinski <alexgadjinski@gmail.com>
2025-04-07 22:52:10 +0300

    implement basic functionality

---
+++ src/main/java/app/user/model/User.java
@@ -0,0 +1,65 @@
+package app.user.model;
+
+import app.subscription.model.Subscription;
+import app.wallet.model.Wallet;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Basic
+    private String firstName;
+
+    @Basic
+    private String lastName;
+
+    @Basic
+    private String profilePicture;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Country country;
+
+    @Basic
+    private boolean isActive;
+
+    @Column(nullable = false)
+    private LocalDateTime createdOn;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedOn;
+
+    @OneToMany(mappedBy = "owner", targetEntity = Subscription.class, fetch = FetchType.EAGER)
+    private List<Subscription> subscriptions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "owner", targetEntity = Wallet.class, fetch = FetchType.EAGER)
+    private List<Wallet> wallets = new ArrayList<>();
+}
