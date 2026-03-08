9cd2ede5c65f572def37e1b07898c25c61fe337e
Aleksandar Gadzhinski <alexgadjinski@gmail.com>
2026-03-08 19:21:22 +0200

    added NotificationServiceFeignCallException

---
+++ src/main/java/app/exception/NotificationServiceFeignCallException.java
@@ -0,0 +1,12 @@
+package app.exception;
+
+public class NotificationServiceFeignCallException extends RuntimeException {
+
+    public NotificationServiceFeignCallException() {
+
+    }
+
+    public NotificationServiceFeignCallException(String message) {
+        super(message);
+    }
+}
