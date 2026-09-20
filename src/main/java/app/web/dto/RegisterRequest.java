09c3bfe0fffde264da0dbfbfa27f5793182003d3
Aleksandar Gadzhinski <alexgadjinski@gmail.com>
2026-09-20 11:59:44 +0300

    fixed validation of RegisterRequest dto

--- src/main/java/app/web/dto/RegisterRequest.java
+++ src/main/java/app/web/dto/RegisterRequest.java
@@ -1,6 +1,7 @@
 package app.web.dto;
 
 import app.user.model.Country;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -10,9 +11,11 @@ import lombok.Data;
 @Data
 public class RegisterRequest {
 
+    @NotBlank
     @Size(min = 6, message = "Username must be at least 6 symbols")
     private String username;
 
+    @NotBlank
     @Size(min = 6, message = "Password must be at least 6 symbols")
     private String password;
 
